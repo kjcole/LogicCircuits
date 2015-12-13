1268664ee7b2ff59add82b909e93676e6d6f10b1
Kevin Cole <dc.loco@gmail.com>
2015-12-13 13:26:05 -0500

    - Added a standard prolog / header to all Python files

--- text.py
+++ text.py
@@ -1,3 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  Animated Circuits
+#
+#  Copyleft 2015 Chris Meyers <chris.meyers47@gmail.com> 2015.12.13
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation; either version 2 of
+#  the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public
+#  License along with this program; if not, write to the Free
+#  Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#  Boston, MA 02110-1301, USA.
+#
+#
+
+from __future__ import print_function
+from six.moves  import input           # use raw_input when I say input
+
+__author__     = "Chris Meyers, Jeff Elkner and Kevin Cole"
+__copyright__  = "Copyleft 2015, Chris Meyers (2015.12.13)"
+__credits__    = ["Chris Meyers", "Jeff Elkner",
+                  "Kevin Cole"]  # Authors and bug reporters
+__license__    = "GPL"
+__version__    = "2.0"
+__maintainer__ = "Chris Meyers, Jeff Elkner, Kevin Cole"
+__email__      = "kevin.cole@novawebcoop.org"
+__status__     = "Development"  # "Prototype", "Development" or "Production"
+__appname__    = "Animated Circuits"
+
 # text.py
 
 import pygame as pg
